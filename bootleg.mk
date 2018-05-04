bb1517109386a1b6afcf8c0875115d74f8131fa8
sahilashraff <sahilashraff@gmail.com>
2018-05-04 08:12:40 +0000

    BootleggersROM Up

--- bootleg.mk
+++ bootleg.mk
@@ -5,12 +5,12 @@ TARGET_SCREEN_HEIGHT := 1920
 # Inherit device configuration
 $(call inherit-product, device/leeco/s2/full_s2.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common BootleggersROM stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := s2
-PRODUCT_NAME := lineage_s2
+PRODUCT_NAME := bootleg_s2
 PRODUCT_BRAND := LeEco
 PRODUCT_MANUFACTURER := LeMobile
 
@@ -27,3 +27,6 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
 
 # Release name
 PRODUCT_RELEASE_NAME := s2
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Sahil Ashraf"
